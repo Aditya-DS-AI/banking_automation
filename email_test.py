4262883fd495b8c83dd34f1c393ece062653b69c
Aditya-DS-AI <115344775+Aditya-DS-AI@users.noreply.github.com>
2025-08-03 17:41:23 +0530

    Add files via upload

---
+++ email_test.py
@@ -0,0 +1,51 @@
+import gmail
+#replace with your email_id & app_pass
+email_id="aditya25datascience@gmail.com"
+app_pass="pdwt jeou xnjg ctlv"
+
+def send_openacn_ack(uemail,uname,uacn,upass):
+    con=gmail.GMail(email_id,app_pass)
+    sub="Congrates😊,Account opened successfully"
+    
+    utext=f"""Hello,{uname}
+Welcome to ABC Bank
+Your Acc No is {uacn}
+Your Pass is {upass}
+Kindly change your password when you login first
+
+Thanks
+ABC Bank
+Noida
+    """
+    msg=gmail.Message(to=uemail,subject=sub,text=utext)
+    con.send(msg)
+
+def send_otp(uemail,otp,amt):
+    con=gmail.GMail(email_id,app_pass)
+    sub="otp for fund transfer"
+    
+    utext=f"""Your otp is {otp} to transfer amount {amt}
+
+Kindly use this otp to complete transfer
+Please don't share to anyone else
+
+Thanks
+ABC Bank
+Noida
+    """
+    msg=gmail.Message(to=uemail,subject=sub,text=utext)
+    con.send(msg)
+
+def send_otp_4_pass(uemail,otp):
+    con=gmail.GMail(email_id,app_pass)
+    sub="otp for password recovery"
+    
+    utext=f"""Your otp is {otp} to recover password
+Please don't share to anyone else
+
+Thanks
+ABC Bank
+Noida
+    """
+    msg=gmail.Message(to=uemail,subject=sub,text=utext)
+    con.send(msg)
